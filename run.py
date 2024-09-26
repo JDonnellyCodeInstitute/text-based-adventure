4312d04f95e5aad727352b4fa992c338f2cc6635
Joe Donnelly <9ulnjm05@students.codeinstitute.net>
2024-09-26 10:05:18 +0000

    Player class, constructor and show stats method added. User input to create character added

--- run.py
+++ run.py
@@ -3,6 +3,11 @@
 import gspread
 from google.oauth2.service_account import Credentials
 
+"""
+Section facilitates API use to update user stats in 
+google sheets
+"""
+
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive.file",
@@ -16,3 +21,28 @@ SHEET = GSPREAD_CLIENT.open('adventurer')
 
 adventurer = SHEET.worksheet('Adventurer')
 print(adventurer.row_values(1))
+
+"""
+Methods for initial set-up and player creation
+"""
+
+class Player:
+    def __init__(self, name, height, sex, gold=10):
+        self.name = name
+        self.height = height
+        self.sex = sex
+        self.gold = gold
+
+    def show_stats(self):
+        print(f"Name: {self.name}")
+        print(f"Height: {self.height}")
+        print(f"Sex: {self.sex}")
+        print(f"Gold: {self.gold} pieces")
+
+# Player creation process from user input
+name = input("Enter your character's name: \n")
+height = input("Enter your character's height: \n")
+sex = input("Enter your character's sex: \n")
+
+player = Player(name, height, sex)
+player.show_stats()
